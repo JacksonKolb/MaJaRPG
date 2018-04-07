8687d39d2ee11d4fcc5ce1aa75e753b2ccf5c960
Jack Kolb <Jack.Kolb@Workiva.com>
2018-04-07 16:56:19 -0600

    created Player.java fields and default constructor

--- Player.java
+++ Player.java
@@ -1,3 +1,24 @@
 public final class Player {
 
+    private final String name;
+    private final String description;
+    private int maxHitPoints;
+    private int hitPoints;
+    private int minDamage;
+    private int maxDamage;
+    private int defense;
+    private double critChance;
+    private Location location;
+
+    public Player(String name, String description, int maxHitPoints, int hitPoints, int minDamage,
+        int maxDamage, int defense, double critChance, Location location) {
+        this.name = name;
+        this.description = description;
+        this.maxHitPoints = maxHitPoints;
+        this.hitPoints = maxHitPoints;
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
+        this.defense = defense;
+        this.critChance = critChance;
+    }
 }
