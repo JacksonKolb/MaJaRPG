3083a27e38cda33d3e2b6bf7685c938fa0100a7f
Jack Kolb <Jack.Kolb@Workiva.com>
2018-04-07 20:46:30 -0600

    Added Item fields and newRandomItem()

--- Item.java
+++ Item.java
@@ -1,3 +1,31 @@
-public final class Item {
+public class Item {
 
+    private String name;
+    private String type;
+    private String description;
+
+    public Item(String name, String type, String description) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+    }
+
+    //TODO - add actual random item calls to potion.java, weapon.java, etc.
+    public static Item newRandomItem() {
+        Item randomItem = null;
+        int random = ProjectMoria.RAND.nextInt(3) + 1;
+        return randomItem;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
