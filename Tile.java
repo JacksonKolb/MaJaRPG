46c752ca8e21b45f8760fda6108ca55b5629c227
Max Morrison <maxdmorrison6@gmail.com>
2018-04-08 15:20:33 -0600

    Added some attributes to Location, Region, and Tile

--- Tile.java
+++ Tile.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
+
 /* Max Morrison & Jackson Kolb
     Tile class handles all of the tile actions for a tile
 */
 
 
 public final class Tile {
+
+    private Region tileRegion;
+    private boolean traversable;
+    private boolean border;
+    private ArrayList<Tile> neighborList;
+
+    public Tile(Region tileRegion, boolean traversable, boolean border ArrayList neighborList){
+
+        this.tileRegion = tileRegion;
+        this.traversable = traversable;
+        this.border = border;
+        this.neighborList = neighborList;
+
+    }
 
 }
