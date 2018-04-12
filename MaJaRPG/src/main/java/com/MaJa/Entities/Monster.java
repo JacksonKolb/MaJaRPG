da3666280a2081a52d862cc2ad0927557aeaf0f2
Andrew Trinh <avqtrinh@gmail.com>
2018-04-12 11:37:46 -0600

    Added Actor Interface and updated Monster, NPC, and player

--- MaJaRPG/src/main/java/com/MaJa/Entities/Monster.java
+++ MaJaRPG/src/main/java/com/MaJa/Entities/Monster.java
@@ -1,16 +1,6 @@
-public final class Monster {
+public final class Monster implements Actor {
 
-    private String name;
-    private String description;
-    private int maxHitPoints;
-    private int hitPoints;
-    private int minDamage;
-    private int maxDamage;
-    private int defense;
-    private double critChance;
     private boolean aggression;
-    private Location location;
-    private Inventory inventory;
 
     public Monster(String name, String description, int maxHitPoints,
             int minDamage, int maxDamage, int defense, double critChance,
@@ -28,47 +18,8 @@ public final class Monster {
         this.inventory = inventory;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getMaxHitPoints() {
-        return maxHitPoints;
-    }
-
-    public int getHitPoints() {
-        return hitPoints;
-    }
-
-    public int getMinDamage() {
-        return minDamage;
-    }
-
-    public int getMaxDamage() {
-        return maxDamage;
-    }
-
-    public int getDefense() {
-        return defense;
-    }
-
-    public double getCritChance() {
-        return critChance;
-    }
-
     public boolean isAggressive() {
         return aggression;
     }
 
-    public Location getLocation() {
-        return location;
-    }
-
-    public Inventory getInventory() {
-        return inventory;
-    }
 }
--- MaJaRPG/src/main/java/com/MaJa/Entities/Npc.java
+++ MaJaRPG/src/main/java/com/MaJa/Entities/Npc.java
@@ -2,6 +2,33 @@
     Npc class handles all of the npc actions
 */
 
-public final class Npc {
+public final class Npc implements Actor {
 
+	 private boolean isQuestGiver;
+	 private boolean isMerchant;
+	 
+	   public Npc(String name, String description, int maxHitPoints,
+	            int minDamage, int maxDamage, int defense, double critChance, boolean isQuestGiver,
+	            boolean isMerchant, Location location, Inventory inventory) {
+	        this.name = name;
+	        this.description = description;
+	        this.maxHitPoints = maxHitPoints;
+	        this.hitPoints = maxHitPoints;
+	        this.minDamage = minDamage;
+	        this.maxDamage = maxDamage;
+	        this.defense = defense;
+	        this.critChance = critChance;
+	        this.isQuestGiver = isQuestGiver;
+	        this.isMerchant = isMerchant;
+	        this.location  = location;
+	        this.inventory = inventory;
+	    }
+	
+	   public boolean isQuestGiver() {
+		   return isQuestGiver;
+	   }
+	   
+	   public boolean isMerchant() {
+		   return isMerchant;
+	   }
 }
