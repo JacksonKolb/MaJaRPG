4f2c02a3b980ace91a61293b7e2d77bb72c3061d
Andrew Trinh <avqtrinh@gmail.com>
2018-04-17 15:43:27 -0600

    added weapon

---
+++ MaJaRPG/src/main/java/com/MaJa/Item/Weapon.java
@@ -0,0 +1,14 @@
+package com.MaJa.Item;
+
+public class Weapon extends Item {
+
+		private int<> modifiers;
+	
+	public Weapon(String name, String type, String description, int<> modifiers) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.modifiers = modifiers;
+    }
+
+}
